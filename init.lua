2b3bf9a7a91830b754298ddcc07ea76ddbf6e73e
Llibert Manent Oristrell <llibert@slimbook.local>
2025-06-07 01:56:17 +0200

    Opcions bàsiques

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -120,6 +120,16 @@ end)
 
 -- Enable break indent
 vim.o.breakindent = true
+-- vim.o.wrap = false
+-- Enable word wrap
+vim.o.linebreak = true
+
+vim.o.colorcolumn = 81
+
+-- Considerar paraules amb les cadenes separades per guionets
+vim.opt.iskeyword:append '-'
+-- Considerar les paraules amb l·l geminada com a paraules úniques
+vim.opt.iskeyword:append '·'
 
 -- Save undo history
 vim.o.undofile = true
